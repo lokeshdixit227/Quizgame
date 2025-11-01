750613fd20661d0c60f49b772a1a81dee92b561b
lokeshdixit227 <lokeshdixit227@gmail.com>
2025-11-01 16:09:10 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,40 +1,95 @@
-questions = [
-    """**Q1:** What is the capital of France?  
-A. Berlin  
-B. Madrid  
-C. Paris  
-D. Rome""",
-    """**Q2:** Which planet is known as the Red Planet?  
-A. Earth  
-B. Mars  
-C. Venus  
-D. Jupiter""",
-    """**Q3:** What is 5 + 3?  
-A. 5  
-B. 8  
-C. 10  
-D. 15""",
-    """**Q4:** What color is the sky?  
-A. Green  
-B. Blue  
-C. Yellow  
-D. Red""",
-    """**Q5:** Which of these is a primary color?  
-A. Green  
-B. Orange  
-C. Purple  
-D. Blue"""
-]
-ans=["a","b","b","b","b"]
-st.write("🧠 Quiz App")
-sum=0
-for i in range (0,5):
-    st.write(questions[i])
-    ua =st.text_input("Your answer:", key=i)  # Each input gets a unique key
-    st.write("---------------------------")  # Adds a line separator between questions
-    if ua==ans[i]:
-        st.balloons()
-        sum=sum+10
+import streamlit as st
+def un():
 
-st.sidebar.button("restart")
-st.write(f"your score is = {sum}")
+def gq():
+    st.write(f"hello Mr. {uname}")
+      st.title("🧠 Quiz App")
+    q= ["""**Q1:** What is the capital of India?
+        A. Mumbai
+        B. Kolkata
+        C. New Delhi
+        D. Chennai""",
+         """**Q2:** Who painted the Mona Lisa?
+        A. Vincent van Gogh
+        B. Leonardo da Vinci
+        C. Pablo Picasso
+        D. Claude Monet""",
+         """**Q3:** What is the chemical symbol for water?
+         A. o2
+         B. co2
+         C. h2o
+         D. nacl""",
+         """**Q4:** Which is the largest continent by land area?
+         A. Africa
+         B. North America
+         C. Asia
+         D. Europe """,
+         """**Q5:** Which of these is a primary color?
+         A. Green
+         B. Orange
+         C. Purple
+         D. Blue """,
+         """**Q6:** What is the currency of the United States?
+         A. Euro
+         B. Yen
+         C. Dollar
+         D. Pound""",
+         """**Q7:** Which planet is known as the Red Planet?
+         A. Earth
+         B. Mars
+         C. Jupiter
+         D. Saturn""",
+         """**Q8:** How many colors are in a rainbow?
+         A. 5
+         B. 6
+         C. 7
+         D. 8""",
+         """**Q9:** Who invented the telephone?
+         a) Thomas Edison
+         b) Alexander Graham Bell
+         c) Nikola Tesla
+         d) Samuel Morse""",
+         """**Q10:** Which festival is known as the Festival of Lights?
+         a) Holi
+         b) Eid
+         c) Diwali
+         d) Christmas"""]
+    ans = ["c", "b", "c", "c", "d", "c", "b", "c", "b", "c"]
+    ua = []
+    sum = 0
+    for i in range(10):
+        st.write(q[i])
+        uas = st.text_input("choose option...",key=i)
+        st.write("---")
+        ua.append(uas)
+        if ua[i] == ans[i]:
+            sum = sum + 10
+            st.balloons()
+    st.write(sum)
+    if sum == 100:
+        st.write("you got 100 points you are topper")
+    elif sum == 90:
+        st.write("you got 90 points you are intermediate")
+    elif sum == 80:
+        st.write("you got 80 points work on your general knowledge")
+    elif sum == 70:
+        st.write("you got 70 points you are topper")
+    elif sum == 60:
+        st.write("you got 60 points you are intermediate")
+    elif sum == 50:
+        st.write("you got 50 points work on your general knowledge")
+    elif sum == 40:
+        st.write("you got 40 points you are topper")
+    elif sum == 30:
+        st.write("you got 30 points you are intermediate")
+    elif sum == 20:
+        st.write("you got 20 points work on your general knowledge")
+    elif sum == 10:
+        st.write("you got 10 points work on your general knowledge")
+    else:
+        st.write("better luck next time")
+
+uname=st.text_input("enter your name")
+# un()
+gq()
+
